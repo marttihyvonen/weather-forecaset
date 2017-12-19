1abe69f31cfcbacbbb6f7da561f61a95cfd58605
Martti Hyvönen <martti.hyvonen@frantic.com>
2017-12-19 17:08:42 +0200

    First version of Weather API integration made.

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const OPEN_WEATHER_MAP_API_KEY = "62e3fca2b41f8736f208f7cf529d45cc";
+const ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?appid=" + OPEN_WEATHER_MAP_API_KEY;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
