0537cf061fa9874e1081ea02efb44ed11d062546
Martti Hyvönen <martti.hyvonen@frantic.com>
2018-02-23 15:39:18 +0200

    Last commit of this exercise.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,6 +4,7 @@ export default function(state = [], action) {
     switch (action.type) {
         case FETCH_WEATHER:
             //return state.concat([action.payload.data]);
+            //below is equal than line above
             return [ action.payload.data , ...state ]; // [city, city, city]
     }
     return state;
